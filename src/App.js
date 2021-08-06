a2fd23d90fe01fc410730426fe1c71f479144f6f
Godwin Nwachukwu <nwgwin@gmail.com>
2021-08-06 12:03:04 +0000

    add logic for updating the number of correct questions

--- src/App.js
+++ src/App.js
@@ -12,13 +12,24 @@ class App extends Component {
     value1: Math.floor(Math.random() * 100),
     value2: Math.floor(Math.random() * 100),
     value3: Math.floor(Math.random() * 100),
-    //proposedAnswer: Math.floor(Math.random() * 3) + this.state.value1 + this.state.value2 + this.state.value3,
-	numQuestions: 0,
+   	numQuestions: 0,
   	numCorrect: 0
+    //correct: false
   };
   
-  proposedAnswer = () => Math.floor(Math.random() * 3) + this.state.value1 + this.state.value2 + this.state.value3;
+  incrementScore = () => {
+    this.setState((currentState)=>({
+      numQuestions: currentState.numQuestions+=1
+    }));
+  }
   
+  numOfCorrectQuestions = () => {
+    this.setState((currentState)=>({
+      numCorrect: currentState.numCorrect+=1
+    }));
+  }
+  
+
   render() {
     return (
       <div className="App">
@@ -31,8 +42,9 @@ class App extends Component {
         <Game 
 		  value1 = {this.state.value1} 
 		  value2 = {this.state.value2} 
-		  value3 = {this.state.value3} 
-		  proposedAnswer = {this.proposedAnswer()} 
+		  value3 = {this.state.value3}
+		  numQuestions = {this.state.numQuestions}
+  		  numCorrect = {this.state.numCorrect}
 		/>
         
       </div>
--- src/Game.js
+++ src/Game.js
@@ -2,25 +2,27 @@ import React, { Component } from 'react'
 
 
 class Game extends Component {
-  
+  //state = {
+    //proposedAnswer: Math.floor(Math.random() * 3) + this.state.value1 + this.state.value2 + this.state.value3,
+  //};
   render(){
     const value1 = this.props.value1;
   	const value2 = this.props.value2;
   	const value3 = this.props.value3;
-  	const proposedAnswer = this.props.proposedAnswer;
-  	const numQuestions = 0;
-  	const numCorrect = 0;
+  	const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+ 	const numQuestions = this.props.numQuestions;
+    const numCorrect = this.props.numCorrect; 
     
     return(
       <div className="game">
         <h2>Mental Math</h2>
         <div className="equation">
-          <p Name="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.proposedAnswer()}`}</p>
+          <p Name="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
         </div>
         <button>True</button>
         <button>False</button>
         <p className="text">
-          Your Score: {this.state.numCorrect}/{this.state.numQuestions}
+          Your Score: {numCorrect}/{numQuestions}
         </p>
       </div>
     )
